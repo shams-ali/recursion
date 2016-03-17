c726970399b8f6232fdcf05e0621a1e4834a771c
Shams Ali <ali.s90@outlook.com>
2016-03-17 13:42:01 -0600

    completed getElementsByClassName with Recursion and Contains

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,8 +1,4 @@
-var _ = require("./underscore.js");
-var jsdom = require("jsdom");
-var window = jsdom.jsdom().defaultView;
-var $ = require("jquery")(window);
-var document = window.document;
+
 // If life was easy, we could just do things the easy way:
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
@@ -11,9 +7,19 @@ var document = window.document;
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
-  var nodeList = [];
-  for(var i = 0; i < array.length; i++){
-    //
+  var result = [];
+  function check(parent, result){ //parent is the document
+    var nodes = parent.childNodes; //gives all elements in document
+    for (var i = 0; i < nodes.length; i++){
+        var classes = nodes[i].classList; //gets all claases in element
+        if (classes && classes.contains(className)) {
+            result.push(nodes[i]);
+        }
+        if (nodes[i].childNodes[0]){
+            check(nodes[i], result);
+        }
+    }
   }
-  return nodeList;
+  check(document, result);
+  return result;
 };
